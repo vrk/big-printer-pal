6a3a92ebe398a984bbe560f345c623c09cacf705
vrk <victoriakirst@gmail.com>
2024-07-22 13:21:07 -0400

    add fabric

--- src/renderer.ts
+++ src/renderer.ts
@@ -29,3 +29,23 @@
 import './css/index.css';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+
+
+import { Canvas } from 'fabric'; // browser
+
+const canvas = new Canvas('html-canvas');
+const DEFAULT_PPI = 300;
+const DEFAULT_WIDTH_IN_INCHES = 8.5;
+const DEFAULT_HEIGHT_IN_INCHES = 11;
+
+let ppi = DEFAULT_PPI;
+let docWidth = DEFAULT_WIDTH_IN_INCHES * ppi;
+let docHeight = DEFAULT_HEIGHT_IN_INCHES * ppi;
+
+const overallContainer = document.getElementById('fabric-canvas-container');
+
+//  edit from here
+canvas.setDimensions({
+  width: overallContainer.offsetWidth,
+  height: overallContainer.offsetHeight,
+});
